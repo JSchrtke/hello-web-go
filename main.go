e1d3cd6c9f91b2faf4acd3e2b7454afa0d0cb405
jschrtke <jschrtke@protonmail.com>
2022-10-28 17:57:46 +0200

    Add basic server

--- main.go
+++ main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World")
+	http.HandleFunc("/", index)
+	http.HandleFunc("/echo", echo)
+	http.HandleFunc("/hello", hello)
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(response http.ResponseWriter, request *http.Request) {
+	sites := []string{url("/hello"), url("/echo")}
+	mustWrite(response, fmt.Sprintf("%s\n", html(list(sites))))
+}
+
+func url(relativePath string) string {
+	return fmt.Sprintf("<a href=\"%s\">%s</a>", relativePath, relativePath)
+}
+
+func html(content string) string {
+	return fmt.Sprintf("<!DOCTYPE html><html><body>%s</body></html>", content)
+}
+
+func list(rows []string) string {
+	var contents string
+	for _, row := range rows {
+		contents += fmt.Sprintf("<li>%s</li>", row)
+	}
+
+	list := fmt.Sprintf("<ul>%s</ul>", contents)
+
+	return list
+}
+
+func echo(response http.ResponseWriter, request *http.Request) {
+	msg := fmt.Sprintf("%#v", request)
+	mustWrite(response, msg)
+
+	fmt.Printf("%#v\n===\n", response)
+}
+
+func hello(response http.ResponseWriter, request *http.Request) {
+	mustWrite(response, "Hello, World")
+}
+
+func mustWrite(response http.ResponseWriter, msg string) {
+	_, err := io.WriteString(response, msg)
+	mustOk(err)
+}
+
+func mustOk(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
